5c14cca3e089ed53da26141eb4364f89e8bafba3
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-14 18:50:57 -0400

    Added custom symbol

--- Window-Shopper/Window-Shopper/View/CurrenyTxtField.swift
+++ Window-Shopper/Window-Shopper/View/CurrenyTxtField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable
 class CurrenyTxtField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLBl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - (size / 2), width: size, height: size))
+        currencyLBl.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        currencyLBl.textAlignment = .center
+        currencyLBl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLBl.layer.cornerRadius = 5.0
+        currencyLBl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLBl.text = formatter.currencySymbol
+        addSubview(currencyLBl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -24,6 +39,8 @@ class CurrenyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.250137544)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor:#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
